b1207d96538c783042c26f895c174df03bc3ee3b
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-12-12 21:57:00 +0100

    Add first basic spec for Match

---
+++ spec/tic_tac_toe/match_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "helper"
+
+RSpec.describe TicTacToe::Match do
+  describe "#current_player" do
+  end
+
+  describe "#start_with" do
+  end
+
+  describe "#next_player" do
+  end
+
+  describe "#game_over?" do
+  end
+end
