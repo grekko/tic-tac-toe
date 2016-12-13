8a7e2e835beedf381c37b07bdd05a1ca0824f47c
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-12-13 22:13:43 +0100

    Make computer player more fun to watch

--- lib/tic_tac_toe/computer_player.rb
+++ lib/tic_tac_toe/computer_player.rb
@@ -13,12 +13,20 @@ class TicTacToe
     end
 
     def make_move
+      cli.clear
+      cli.print turn_message
+      sleep 2
       board.update field: board.empty_fields.first, symbol: symbol
     end
 
     def to_s
       "Player #{number} (#{symbol}, Computer)"
     end
+
+    private
+
+    def turn_message
+      "#{self}, its your turn.\n\nThe board:\n===========\n#{board}"
     end
   end
 end
