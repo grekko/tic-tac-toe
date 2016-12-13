ebdf724b8483be4acb697cca7aa811a306841909
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-12-13 21:21:55 +0100

    Extend HumanPlayer spec

--- spec/tic_tac_toe/human_player_spec.rb
+++ spec/tic_tac_toe/human_player_spec.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal: true
 
 require "helper"
-require "stringio"
 
 RSpec.describe TicTacToe::HumanPlayer do
-  subject { described_class.new symbol: "X", stdin: stdin, stdout: stdout }
-  let(:stdin)  { StringIO.new }
-  let(:stdout) { StringIO.new }
+  subject     { described_class.new number: 1, symbol: "X", cli: cli }
+  let(:cli)   { instance_double("TicTacToe::Cli", clear: true, print: true) }
+  let(:board) { instance_double("TicTacToe::Board", empty_fields: []) }
+
+  before do
+    subject.board = board
+  end
+
+  describe "#make_move" do
+    it "asks the human player via cli for the next move" do
+      expect(cli).to receive(:ask).and_return("1")
+      expect(board).to receive(:update).with(field: "1", symbol: subject.symbol)
+      subject.make_move
+    end
+  end
+
+  describe "#to_s" do
+    it "displays the Players name properly" do
+      expect(subject.to_s).to eq("Player 1 (Human)")
+    end
+  end
 end
